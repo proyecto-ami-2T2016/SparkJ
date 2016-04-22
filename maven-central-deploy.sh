0cd7f9dabec5b4abe20ac519c87ab95f6ff3dc70
Grant West <g.west@me.com>
2016-04-21 19:55:52 -0500

    run ossrh profile

--- maven-central-deploy.sh
+++ maven-central-deploy.sh
@@ -1,11 +1,11 @@
-!/bin/bash
+#!/bin/bash
 # Deploy maven artefact in current directory into Maven central repository
 # using maven-release-plugin goals
 read -p "Really deploy to maven central repository  (yes/no)? "
 if ( [ "$REPLY" == "yes" ] ) then
   ssh-add
   ssh-add -l
-  mvn release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
+  mvn release:clean release:prepare release:perform -P ossrh -B -e | tee maven-central-deploy.log
   ssh-add -D
 else
   echo 'Exit without deploy'
